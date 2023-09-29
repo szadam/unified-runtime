0f9347a59737930d1685f68a488673ea70e6e431
Adam <szadamx@gmail.com>
2023-09-29 11:31:05 +0200

    [UR][Tests] Print all CTS outputs (from gtest) in CI

--- test/conformance/cts_exe.py
+++ test/conformance/cts_exe.py
@@ -7,6 +7,8 @@
  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 """
+# Writing conformance test output from gtest to a file and checking failed tests with match files.
+# The match files contain tests that are expected to fail.
 
 import sys
 from argparse import ArgumentParser
@@ -18,18 +20,24 @@ if __name__ == '__main__':
 
     parser = ArgumentParser()
     parser.add_argument("--test_command", help="Ctest test case")
-
     args = parser.parse_args()
-    result = subprocess.Popen([args.test_command, '--gtest_brief=1'], stdout = subprocess.PIPE, text = True)  # nosec B603
+    result = subprocess.Popen([args.test_command, '--gtest_brief=1'], stdout=subprocess.PIPE,
+                              stderr=subprocess.STDOUT, text=True)  # nosec B603
 
     pat = re.compile(r'\[( )*FAILED( )*\]')
-    for line in result.stdout.readlines():
+    output_list = []
+    for line in result.stdout:
+        output_list.append(line)
         if pat.search(line):
             test_case = line.split(" ")[5]
             test_case = test_case.rstrip(',')
             print(test_case)
 
-    result.communicate()
-    rc = result.returncode
+    print("#### OUTPUT ####", file=sys.stderr)
+    for output in output_list:
+        print(output, file=sys.stderr)
+    print("#### OUTPUT_END ####", file=sys.stderr)
+
+    rc = result.wait()
     if rc < 0:
-        print(signal.strsignal(abs(result.returncode)))
+        print(signal.strsignal(abs(rc)))
