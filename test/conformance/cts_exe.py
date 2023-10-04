e31cb1a90bc034a5c893fd3718f43e6f1955a794
Adam <szadamx@gmail.com>
2023-10-04 15:55:42 +0200

    Add diffrent way to print CTS output from gtest

--- test/conformance/cts_exe.py
+++ test/conformance/cts_exe.py
@@ -17,15 +17,14 @@ import signal
 import re
 
 if __name__ == '__main__':
-
     parser = ArgumentParser()
     parser.add_argument("--test_command", help="Ctest test case")
 
     args = parser.parse_args()
-    output_file = open("output.txt", "w")
 
-    result = subprocess.Popen([args.test_command, '--gtest_brief=1'], stdout=subprocess.PIPE,
-                              stderr=subprocess.STDOUT, text=True)  # nosec B603
+    result = subprocess.Popen(
+        f"{args.test_command} --gtest_brief=1 2>&1 | tee /dev/tty", shell=True, text=True
+    )  # nosec B603
 
     pat = re.compile(r'\[( )*FAILED( )*\]')
     for line in result.stdout:
@@ -33,10 +32,8 @@ if __name__ == '__main__':
             test_case = line.split(" ")[5]
             test_case = test_case.rstrip(',')
             print(test_case)
-        output_file.write(line)
 
     rc = result.wait()
-    output_file.close()
 
     if rc < 0:
         print(signal.strsignal(abs(rc)))
